a0e6957522dded851e90da0bfc9608c75dd0435f
Darcy Cifuentes <lisethcifuentes20@gmail.com>
2026-05-06 21:19:27 +0800

    Implementacion de useNavigate, boton Ver servicios

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,10 +1,12 @@
 //import React from 'react'
 import './Section1.css'
 import React, { useState } from 'react' /* Importamos el Hook useState */
+import { useNavigate } from 'react-router-dom'; /* Importamos el Hook de navegacion */
 
 function Section1() {
 
   const [nombre, setNombre] = useState("");
+  const navigate = useNavigate();
 
   return (
     <div>
@@ -20,12 +22,22 @@ function Section1() {
           <p>Usuario actual <strong>{nombre}</strong></p> {/* Mostramos el nombre actual en pantalla */}
         </div>
 
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error sint modi voluptatem possimus aliquam repellendus in suscipit officia distinctio nostrum, aliquid hic totam pariatur quis voluptate rerum! Laborum, omnis esse.</p>
-
-        <h2>Mas informacion</h2> {/* Creamos un subtitulo */}
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae eius itaque accusantium provident, perferendis consequuntur ducimus enim ipsa? Soluta suscipit, animi hic unde iste laboriosam corporis recusandae tempore omnis at.</p>
+        <div className='caja_navegacion'>
+          <p>Quieres conocer nuestros servicios?</p>
+          <button className='boton_navegar' onClick={() => navigate ("servicios")}>
+            Ver servicios
+          </button>
+        </div>
+        
+        <div>
+          <h2>Informacion General</h2>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error sint modi voluptatem possimus aliquam repellendus in suscipit officia distinctio nostrum, aliquid hic totam pariatur quis voluptate rerum! Laborum, omnis esse.</p>
+          
+          <h2>Mas informacion</h2> {/* Creamos un subtitulo */}
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae eius itaque accusantium provident, perferendis consequuntur ducimus enim ipsa? Soluta suscipit, animi hic unde iste laboriosam corporis recusandae tempore omnis at.</p>
+        </div>
     </div>    
   )
 }
 
-export default Section1
+export default Section1;
