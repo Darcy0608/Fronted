e637222e6f8da5a6ca63f631bfe9bc2fb61f46bc
Darcy Cifuentes <lisethcifuentes20@gmail.com>
2026-05-06 11:57:32 +0800

    Implementacion componente, almacena nombre de un usuario

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -1,14 +1,37 @@
 //import React from 'react'
 import "./Section2.css"
+import React, { useState, useEffect } from 'react' /* Importamos el Hook useState */
+
 
 const Section2 = () => {
+  const [cargando, setCargando] = useState(true);
+
+  useEffect(() =>{
+    console.log("El componente Section2 se a cargado");
+
+    const timer = setTimeout(() => {
+      setCargando(false);
+      console.log("Carga de datos simulada completa");
+    }, 2000);
+
+    return () => clearTimeout(timer); 
+  }, []);
+
+
   return (
+    
     <div className='section2'>
       <h2>Segunda seccion</h2>
-
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate in illum sed maxime, a nostrum modi quo voluptas earum soluta porro laborum! Odit nemo dolore itaque. Nesciunt quasi deleniti officia.</p>
+      {cargando ? (
+        <p><strong>Cargando servicios...</strong></p>
+      ) : (
+        <div>
+          <p>Datos recibidos correctamente.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate in illum sed maxime, a nostrum modi quo voluptas earum soluta porro laborum! Odit nemo dolore itaque. Nesciunt quasi deleniti officia.</p>
     </div>
+      )}
+      </div>
   )
 }
 
-export default Section2
+export default Section2;
